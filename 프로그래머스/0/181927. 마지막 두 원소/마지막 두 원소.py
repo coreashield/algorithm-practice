7e6b9533c024d13496f5be442c5024be7890ec31
coreashield <120443729+coreashield@users.noreply.github.com>
2024-10-14 00:13:30 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    answer = []
+    for i in range(len(num_list)):
+        answer.append(num_list[i])
+        if i == len(num_list)-1:
+            if num_list[i-1] < num_list[i]:
+                answer.append(num_list[i] - num_list[i-1])
+            else:
+                answer.append(num_list[i] * 2)
+                    
+            
+    
+        
+    return answer
