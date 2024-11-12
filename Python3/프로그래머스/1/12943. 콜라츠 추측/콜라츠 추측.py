88acb7ecc3eeefc351b89ebc2036e9d5d11dc675
coreashield <120443729+coreashield@users.noreply.github.com>
2024-11-12 09:36:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.29 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    index = 1
+    if num == 1:
+        return 0
+    while( num > 1 ):
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+            
+        if num == 1:
+            return index
+        elif index > 500:
+            return -1
+        index += 1
+        
